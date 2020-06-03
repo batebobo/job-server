bc1856ae8e74a1f498c47caf4b32e5a083622f3c
Boyan Vushkov <bvushkov@gmail.com>
2020-06-03 19:54:52 +0300

    Responses depend on the "Accept" header
    If the client requests plain text, the server returns a bash script
    with all commands from the tasks
    
    Otherwise a JSON is returned

--- lib/router.ex
+++ lib/router.ex
@@ -4,7 +4,7 @@ defmodule JobServer.Router do
   plug(:match)
   plug(:dispatch)
 
-  defp process(tasks_map) do
+  defp get_correct_task_order(tasks_map) do
     tasks = Enum.map(tasks_map, fn task ->
       %Job{
         name: Map.get(task, "name"),
@@ -14,16 +14,43 @@ defmodule JobServer.Router do
     end)
     graph = DependencyGraph.new(tasks)
     case DependencyGraph.topological_sort(graph) do
-      {:ok, result} -> {:ok, Poison.encode!(Enum.map(result, &(&1.job)))}
-      {:error, message} -> {:error, Poison.encode!(%{response: message})}
+      {:ok, result} -> {:ok, Enum.map(result, &(&1.job))}
+      {:error, message} -> {:error, %{response: message}}
     end
+  end
+
+  defp encode(result, response_type) do
+    is_plain_text_requested = response_type == "text/plain"
+    is_json_requested = response_type == "application/json"
+    case result do
+      {:error, message} when is_json_requested ->
+        {:error, Poison.encode!(%{error: message})}
+      {:ok, response} when is_plain_text_requested ->
+        {:ok, get_script_from_tasks(response)}
+      {:ok, response} ->
+        {:ok, Poison.encode!(response)}
+    end
+  end
+
+  defp get_script_from_tasks(tasks) do
+    tasks_commands = Enum.map(tasks, &(&1.command))
+    header = "#!/usr/bin/env bash \n"
+    Enum.concat([header], tasks_commands)
+      |> Enum.join("\n")
+  end
+
+  defp process(tasks, conn) do
+    response_type = Enum.into(conn.req_headers, Map.new)
+      |> Map.get("accept")
+    get_correct_task_order(tasks)
+      |> encode(response_type)
   end
 
   post "/" do
     {status, body} =
       case conn.body_params do
         %{"tasks" => tasks} ->
-          case process(tasks) do
+          case process(tasks, conn) do
             {:ok, response} -> {200, response}
             {:error, error_message} -> {400, error_message}
           end
