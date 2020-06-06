796ccda8288ea1ea434aa1e23889bc7181e75871
Boyan Vushkov <bvushkov@gmail.com>
2020-06-06 13:17:35 +0300

    Add @docs and @specs

--- lib/task_order_router.ex
+++ lib/task_order_router.ex
@@ -1,12 +1,17 @@
 defmodule JobServer.TaskOrderRouter do
+  @moduledoc """
+  Documentation for `TaskOrderRouter`.
+  Handles requests for task reordering
+  """
   use Plug.Router
 
   plug(:match)
   plug(:dispatch)
 
+  @spec format_cycles(list(list(String.t()))) :: String.t()
   defp format_cycles(cycles) do
-    Enum.map(cycles, &(Enum.join(&1, "->")))
-      |> Enum.join(", ")
+    Enum.map(cycles, &Enum.join(&1, " -> "))
+    |> Enum.join(", ")
   end
 
   defp cycles_error_message do
@@ -21,64 +26,83 @@ defmodule JobServer.TaskOrderRouter do
     "The following tasks have incorrect format"
   end
 
+  @spec incorrect_tasks_error_response(any) :: {String.t(), String.t()}
   defp incorrect_tasks_error_response(incorrect_tasks) do
     message = "#{incorrect_input_error_message()}. The listed task requirements do not exist:"
     {message, Poison.encode!(incorrect_tasks)}
   end
 
-  defp get_correct_order(for: tasks_map) do
+  defp convert_to_struct(tasks_map) do
     {correct_tasks, incorrect_tasks} = Enum.reduce(tasks_map, {[], []}, fn task, {correct, incorrect} ->
       if Map.has_key?(task, "name") && Map.has_key?(task, "command") do
         new_task = %JobTask{
-            name: Map.get(task, "name"),
-            command: Map.get(task, "command"),
-            requires: Map.get(task, "requires", [])
-          }
+          name: Map.get(task, "name"),
+          command: Map.get(task, "command"),
+          requires: Map.get(task, "requires", [])
+        }
+
         {Enum.concat(correct, [new_task]), incorrect}
       else
         {correct, Enum.concat(incorrect, [task])}
       end
     end)
+    case incorrect_tasks do
+      [] -> {:ok, correct_tasks}
+      _  -> {:missing_properties_error, incorrect_tasks}
+    end
+  end
 
-    if not Enum.empty? incorrect_tasks do
-      {:error, :incorrect_tasks_error, {incorrect_input_error_message(), Poison.encode!(incorrect_tasks)}}
+  defp get_correct_order(for: tasks_map) do
+    with {:ok, correct_tasks} <- convert_to_struct(tasks_map),
+         {:ok, reordered_tasks} <- TaskOrderService.get_tasks_order(correct_tasks)
+    do
+       {:ok, reordered_tasks}
     else
-      case TaskOrderService.get_tasks_order(correct_tasks) do
-        {:cyclic_tasks_error, cycles} -> {:error, :cyclic_tasks_error, cycles_error_response(cycles)}
-        {:incorrect_tasks_error, incorrect_tasks} ->
-          {:error, :incorrect_tasks_error, incorrect_tasks_error_response(incorrect_tasks)}
-        result -> result
-      end
+      {:missing_properties_error, incorrect_tasks} -> {:error, :incorrect_tasks_error,
+         {incorrect_input_error_message(), Poison.encode!(incorrect_tasks)}}
+
+      {:cyclic_tasks_error, cycles} ->
+            {:error, :cyclic_tasks_error, cycles_error_response(cycles)}
+
+      {:incorrect_tasks_error, incorrect_tasks} ->
+            {:error, :incorrect_tasks_error, incorrect_tasks_error_response(incorrect_tasks)}
     end
   end
 
   defp encode(result, with: response_type) do
     is_plain_text_requested = response_type == "text/plain"
     is_json_requested = response_type == "application/json"
+
     case result do
       {:error, error_type, {message, payload}} when is_json_requested ->
         {error_type, Poison.encode!(%{error: message, payload: payload})}
+
       {:error, error_type, {message, payload}} ->
         {error_type, "Error: #{message}. Payload: #{payload}"}
+
       {:ok, response} when is_plain_text_requested ->
         {:ok, get_script_from_tasks(response)}
+
       {:ok, response} ->
         {:ok, Poison.encode!(response)}
     end
   end
 
   defp get_script_from_tasks(tasks) do
-    tasks_commands = Enum.map(tasks, &(&1.command))
+    tasks_commands = Enum.map(tasks, & &1.command)
     header = "#!/usr/bin/env bash \n"
+
     Enum.concat([header], tasks_commands)
-      |> Enum.join("\n")
+    |> Enum.join("\n")
   end
 
   defp process(tasks, conn) do
-    response_type = Enum.into(conn.req_headers, Map.new)
+    response_type =
+      Enum.into(conn.req_headers, Map.new())
       |> Map.get("accept")
+
     get_correct_order(for: tasks)
-      |> encode(with: response_type)
+    |> encode(with: response_type)
   end
 
   post "/" do
@@ -90,8 +114,11 @@ defmodule JobServer.TaskOrderRouter do
             {:incorrect_tasks_error, error_message} -> {422, error_message}
             {:cyclic_tasks_error, error_message} -> {400, error_message}
           end
-        _ -> {422, Poison.encode!(%{error: "Expected Payload: { 'tasks': [...] }"})}
+
+        _ ->
+          {422, Poison.encode!(%{error: "Expected Payload: { 'tasks': [...] }"})}
       end
-      send_resp(conn, status, body)
+
+    send_resp(conn, status, body)
   end
 end
--- lib/task_order_service.ex
+++ lib/task_order_service.ex
@@ -1,60 +1,101 @@
 defmodule TaskOrderService do
+  @moduledoc """
+  Documentation for `TaskOrderService`.
+  A service that is can reorder a collection of tasks
+  so that every task is after its requirements
+  """
+
+  @spec contains(list(any), any):: boolean
   defp contains(enum, item) do
     Enum.any?(enum, &(&1 == item))
   end
 
+  @spec add_edges(:digraph.graph(), list(JobTask)) :: :digraph.graph()
   defp add_edges(graph, tasks) do
     Enum.each(tasks, fn task ->
-      Enum.each(task.requires, &(:digraph.add_edge(graph, &1, task.name)))
+      Enum.each(task.requires, &:digraph.add_edge(graph, &1, task.name))
     end)
+
     graph
   end
 
-  defp new(tasks) do
-    graph = Enum.reduce(tasks, :digraph.new(), fn task, graph ->
-      :digraph.add_vertex(graph, task.name)
-      graph
-    end)
-
-    incorrect_tasks = Enum.reduce(tasks, [], fn task, incorrect_tasks ->
+  @spec missing_requirements(:digraph.graph(), list(JobTask)) :: list(any)
+  defp missing_requirements(graph, tasks) do
+    Enum.reduce(tasks, [], fn task, incorrect_tasks ->
       case Enum.filter(task.requires, &(not contains(:digraph.vertices(graph), &1))) do
-        [] -> incorrect_tasks
-        incorrect_dependencies -> Enum.concat(incorrect_tasks,
-          [%{task: task, missing_requirements: incorrect_dependencies}])
+        [] ->
+          incorrect_tasks
+
+        incorrect_dependencies ->
+          Enum.concat(
+            incorrect_tasks,
+            [%{task: task, missing_requirements: incorrect_dependencies}]
+          )
       end
     end)
+  end
 
-    case incorrect_tasks do
+  @spec new(list(JobTask)) ::
+   {:ok, :digraph.graph()} |
+   {:incorrect_tasks_error, any}
+  defp new(tasks) do
+    graph = :digraph.new()
+    Enum.each(tasks, &:digraph.add_vertex(graph, &1.name))
+
+    case missing_requirements(graph, tasks) do
       [] -> {:ok, add_edges(graph, tasks)}
-      _  -> {:incorrect_tasks_error, incorrect_tasks}
+      missing_requirements -> {:incorrect_tasks_error, missing_requirements}
     end
   end
 
+  @spec find_cycles(:digraph.graph()):: {:ok, list(any)} | {:cyclic_tasks_error, list(any)}
   defp find_cycles(graph) do
-    cycles = Enum.reduce(:digraph.vertices(graph), [], fn vertex, cycles ->
-      case :digraph.get_short_cycle(graph, vertex) do
-        false -> cycles
-        cycle -> Enum.concat(cycles, [cycle])
-      end
-    end)
+    cycles =
+      Enum.reduce(:digraph.vertices(graph), [], fn vertex, cycles ->
+        case :digraph.get_short_cycle(graph, vertex) do
+          false -> cycles
+          cycle -> Enum.concat(cycles, [cycle])
+        end
+      end)
+
     case cycles do
       [] -> {:ok, []}
-      _  -> {:cyclic_tasks_error, cycles}
+      _ -> {:cyclic_tasks_error, cycles}
     end
   end
 
+  @doc """
+  Reorders `tasks` so that each task is executed after
+  its requirements.
+
+  Returns `{:ok, tasks}`
+
+  Returns `{:cyclic_tasks_error, cycles}` if there are cycles in the dependency graph
+
+  Returns `{:incorrect_tasks_error, incorrect_tasks}` if some of the task have missing requirements
+  """
+  @spec get_tasks_order(list(JobTask))::
+    {:ok, list(JobTask)} |
+    {:cyclic_tasks_error, list(any)} |
+    {:incorrect_tasks_error, list(any)}
   def get_tasks_order(tasks) do
     with {:ok, graph} <- new(tasks),
-         {:ok, []}    <- find_cycles(graph)
-    do
+         {:ok, []} <- find_cycles(graph) do
       get_tasks_order(tasks, graph)
     end
   end
 
+  # Used to retrieve all task information ordered by the `task_names`
+  @spec get_tasks(list(JobTask), list(String.t())):: list(JobTask)
   defp get_tasks(tasks, task_names) do
-    Enum.map(task_names, &(Enum.find(tasks, fn task -> task.name == &1 end)))
+    Enum.map(task_names, &Enum.find(tasks, fn task -> task.name == &1 end))
   end
 
+  # Performs topological sort on the given `graph` and extracts
+  # all tasks data
+  @spec get_tasks_order(list(JobTask), :digraph.graph())::
+    {:ok, list(JobTask)} |
+    {:cyclic_tasks_error, list(any)}
   defp get_tasks_order(tasks, graph) do
     case :digraph_utils.topsort(graph) do
       false -> {:cyclic_tasks_error, []}
